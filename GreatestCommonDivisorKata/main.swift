eb424c8ff3f860683fca72cc168dcbd564510ecb
Ealden Esto E. Escañan <ealden@escanan.com>
2014-06-17 18:29:53 +0800

    Initial Commit

---
+++ GreatestCommonDivisorKata/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  GreatestCommonDivisorKata
+//
+//  Created by Ealden Escañan on 6/17/14.
+//  Copyright (c) 2014 Ealden Escañan. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)

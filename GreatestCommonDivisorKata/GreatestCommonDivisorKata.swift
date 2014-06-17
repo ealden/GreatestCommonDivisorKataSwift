9279e7d88ec112f4fedfb0eff09c55ea07f0cb75
Ealden Esto E. Escañan <ealden@escanan.com>
2014-06-17 18:36:32 +0800

    Initial GreatestCommonDivisorKata

---
+++ GreatestCommonDivisorKata/GreatestCommonDivisorKata.swift
@@ -0,0 +1,13 @@
+//
+//  GreatestCommonDivisorKata.swift
+//  GreatestCommonDivisorKata
+//
+//  Created by Ealden Escañan on 6/17/14.
+//  Copyright (c) 2014 Ealden Escañan. All rights reserved.
+//
+
+import Foundation
+
+class GreatestCommonDivisorKata {
+    
+}

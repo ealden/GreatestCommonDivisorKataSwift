da56eccb8c14cfd0e8cec83d05757050d6a790c3
Ealden Esto E. Escañan <ealden@escanan.com>
2014-06-21 00:10:07 +0800

    XCTAssertEquals is actual, expected

--- GreatestCommonDivisorKataTests/GreatestCommonDivisorKataTests.swift
+++ GreatestCommonDivisorKataTests/GreatestCommonDivisorKataTests.swift
@@ -12,40 +12,40 @@ class GreatestCommonDivisorKataTests: XCTestCase {
     let gcd = GreatestCommonDivisorKata()
 
     func testShouldReturnNumberIfInputsAreTheSame() {
-        XCTAssertEqual(1, gcd.calculate(1, 1))
-        XCTAssertEqual(2, gcd.calculate(2, 2))
+        XCTAssertEqual(gcd.calculate(1, 1), 1)
+        XCTAssertEqual(gcd.calculate(2, 2), 2)
     }
 
     func testShouldReturnSmallerInputIfDivisibleToOtherInput() {
-        XCTAssertEqual(2, gcd.calculate(4, 2))
-        XCTAssertEqual(2, gcd.calculate(2, 4))
+        XCTAssertEqual(gcd.calculate(4, 2), 2)
+        XCTAssertEqual(gcd.calculate(2, 4), 2)
     }
 
     func testShouldReturn1IfInputsAreBothPrime() {
-        XCTAssertEqual(1, gcd.calculate(3, 7))
-        XCTAssertEqual(1, gcd.calculate(11, 13))
+        XCTAssertEqual(gcd.calculate(3, 7), 1)
+        XCTAssertEqual(gcd.calculate(11, 13), 1)
     }
 
     func testShouldReturnGCD() {
-        XCTAssertEqual(6, gcd.calculate(54, 24))
-        XCTAssertEqual(6, gcd.calculate(24, 54))
-        XCTAssertEqual(9, gcd.calculate(27, 90))
+        XCTAssertEqual(gcd.calculate(54, 24), 6)
+        XCTAssertEqual(gcd.calculate(24, 54), 6)
+        XCTAssertEqual(gcd.calculate(27, 90), 9)
     }
 
     func testShouldReturn1IfAnInputIsO() {
-        XCTAssertEqual(1, gcd.calculate(0, 20))
+        XCTAssertEqual(gcd.calculate(0, 20), 1)
     }
 
     func testShouldReturn1IfBothInputs0() {
-        XCTAssertEqual(1, gcd.calculate(0, 0))
+        XCTAssertEqual(gcd.calculate(0, 0), 1)
     }
 
     func testShouldReturn0IfAnInputIsNegative() {
-        XCTAssertEqual(0, gcd.calculate(1, -1))
-        XCTAssertEqual(0, gcd.calculate(-1, 1))
+        XCTAssertEqual(gcd.calculate(1, -1), 0)
+        XCTAssertEqual(gcd.calculate(-1, 1), 0)
     }
 
     func testShouldReturn0IfBothInputsNegative() {
-        XCTAssertEqual(0, gcd.calculate(-20, -35))
+        XCTAssertEqual(gcd.calculate(-20, -35), 0)
     }
 }
